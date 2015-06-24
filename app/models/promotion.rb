e183ee4b01f65aa110a4618890c0173ff63dcf42
Jordan Brough <git.j@brgh.net>
2015-06-24 08:57:31 -0600

    Some quick test code

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,10 @@
 class Promotion < ActiveRecord::Base
   belongs_to :order
   belongs_to :promotion_category
+
+  def self.test
+    promotion_category = Spree::PromotionCategory.create!(name: 'some spree promo category')
+    promotion = Spree::Promotion.create!(name: 'some spree promo', promotion_category: promotion_category)
+    Spree::Promotion.find(promotion.id).promotion_category
+  end
 end
